5a53fe2192a80b10420c1a77ba46b8b2197a98d0
liam.docherty <liam.docherty@solarwinds.com>
2020-02-18 18:16:07 +0000

    Get basic login test working that clicks button

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -9,15 +9,29 @@ import java.util.List;
 public class LoginPage {
 
     private WebDriver driver;
-    private WebElement loginForm = driver.findElement(By.id("login_form"));
-    private WebElement loginFormHeading = loginForm.findElement(By.tagName("h3"));
-    private List<WebElement> loginFormFields = loginForm.findElements(By.className("form-group"));
-    private WebElement loginFormEmail = loginFormFields.get(0);
-    private WebElement loginFormEmailLabel = loginFormEmail.findElement(By.tagName("label"));
-    private WebElement loginFormEmailInput = driver.findElement(By.id("email"));
+    private WebElement loginForm;
+    private WebElement loginFormHeading;
+    private List<WebElement> loginFormFields;
+    private WebElement loginFormEmail;
+    private WebElement loginFormEmailLabel;
+    private WebElement loginFormEmailInput;
+    private WebElement loginFormPassword;
+    private WebElement loginFormPasswordLabel;
+    private WebElement loginFormPasswordInput;
+    private WebElement signInButton;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        this.loginForm = driver.findElement(By.id("login_form"));
+        this.loginFormHeading = loginForm.findElement(By.tagName("h3"));
+        this.loginFormFields = loginForm.findElements(By.className("form-group"));
+        this.loginFormEmail = loginFormFields.get(0);
+        this.loginFormEmailLabel = loginFormEmail.findElement(By.tagName("label"));
+        this.loginFormEmailInput = driver.findElement(By.id("email"));
+        this.loginFormPassword = loginFormFields.get(1);
+        this.loginFormPasswordLabel = loginFormPassword.findElement(By.tagName("label"));
+        this.loginFormPasswordInput = driver.findElement(By.id("passwd"));
+        this.signInButton = driver.findElement(By.id("SubmitLogin"));
     }
 
     public String getLoginEmailLabel() {
@@ -27,5 +41,9 @@ public class LoginPage {
     public void enterLoginEmail(String text) {
         loginFormEmailInput.sendKeys(text);
     }
+
+    public void enterLoginPassword(String text) { loginFormPasswordInput.sendKeys(text); }
+
+    public void clickSignIn() { signInButton.click(); }
 
 }
--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -6,10 +6,10 @@ import org.testng.annotations.Test;
 public class LoginTests extends BaseTests {
 
     @Test
-    public void test123() {
-        setUp();
-        System.out.println(loginPage.getLoginEmailLabel());
-        loginPage.enterLoginEmail("This is a test");
+    public void successfulLogin() {
+        loginPage.enterLoginEmail("ldtest1@test.com");
+        loginPage.enterLoginPassword("Test1234");
+        loginPage.clickSignIn();
     }
 
 }

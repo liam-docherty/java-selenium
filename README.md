0c43c463fb0e784f48177e90153bb39898e30532
liam.docherty <liam.docherty@solarwinds.com>
2020-02-23 14:24:14 +0000

    Get test running using mvn test

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 ## TODO
 Include example of dropdown page object interactions. "Sort by" is an example of this: http://automationpractice.com/index.php?id_category=3&controller=category
 
+## Run Tests
+To run all tests run `mvn test` in the command line.
+
 ## Screenshots
 A screenshot is only taken when a test fails.<p>
 The screenshot will be saved to [`/resources/screenshots`](/resources/screenshots).<p>
--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,11 +1,13 @@
+package runner;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/java/features",
-        glue = {"steps"},
-        tags = {"~@Ignore"},
-        plugin = {"pretty"}
+        features = {"src/test/java/features"},
+        glue = {"steps"}
+//        tags = {"~@Ignore"},
+//        plugin = {"pretty"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
